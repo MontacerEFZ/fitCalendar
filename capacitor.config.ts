f26d6d9031526b18382d8e3254d95d837dd0b8f5
montacer <montacer.elfazazielfazazi@cieep.com>
2024-02-15 15:39:54 +0100

    good

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'FitCalendar',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
